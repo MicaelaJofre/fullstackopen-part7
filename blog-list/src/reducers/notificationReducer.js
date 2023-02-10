1286306c2599fb1664971b0cfc83adc4c2872286
Micaela Analia Jofre <mkjofre@gmail.com>
2023-02-10 17:46:00 -0300

    created notification reducer

--- blog-list/src/reducers/notificationReducer.js
+++ blog-list/src/reducers/notificationReducer.js
@@ -1,3 +1,27 @@
-export const notificationReducer = () => {
+import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = { message: '', status: '', timeout: null }
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState,
+    reducers: {
+        setNotification(state, action) {
+            state.timeout && clearTimeout(state.timeout)
+            return action.payload
+        },
+    }
+})
+
+const { setNotification } = notificationSlice.actions
+
+export const createNotification = (message, status, time) => {
+    console.log(time)
+    return async dispach => {
+        const timeout = setTimeout(() => { dispach(setNotification(initialState)) }, time * 1000)
+        dispach(setNotification({ message, status, timeout }))
+    }
 }
+
+
+export default notificationSlice.reducer
