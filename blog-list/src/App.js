e36128e8d36cb459c0785871e42a6674531c62b3
Micaela Analia Jofre <mkjofre@gmail.com>
2023-02-14 23:03:43 -0300

    Create Routes and useMatch

--- blog-list/src/App.js
+++ blog-list/src/App.js
@@ -1,66 +1,49 @@
-import { useEffect } from 'react'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getBlogs } from './reducers/blogsReducer'
+import { initialUser } from './reducers/userReducer'
+import { initializeUsers } from './reducers/usersReducer'
+import UserListBlogs from './components/UserListBlogs'
+import Users from './components/Users'
+import Menu from './components/Menu'
 import Blog from './components/Blog'
-import LoginForm from './components/LoginForm'
-import BlogForm from './components/BlogForm'
-import Notification from './components/Notification'
-import { getBlogs, addBlog, blogLikes, blogDelete } from './reducers/blogsReducer'
-import { initialUser, loginUser } from './reducers/userReducer'
+import BlogsList from './components/BlogsList'
 import './main.css'
 
 const App = () => {
     const blogs = useSelector(state => [...state.blogs].sort((a, b) => b.likes - a.likes))
-    const user = useSelector(state => state.user)
+    const users = useSelector(state => state.users)
     const dispatch = useDispatch()
-
 
     useEffect(() => {
         dispatch(getBlogs())
         dispatch(initialUser())
+        dispatch(initializeUsers())
     }, [])
 
-    const addUser = (userObject) => {
-        dispatch(loginUser(userObject))
+    const match = useMatch('/users/:id')
+    const blogsUser = match
+        ? users.find(user => user.id === match.params.id)
+        : null
 
-    }
-
-    const handleCreate = (blogObject) => {
-        dispatch(addBlog(blogObject))
-    }
-
-    const handleLikes = (blog) => {
-        dispatch(blogLikes(blog.id))
-    }
-
-    const handleDelete = (blog) => {
-        if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-            dispatch(blogDelete(blog.id))
-        }
-    }
-
-
+    const matchBlogs = useMatch('/blogs/:id')
+    const blog = matchBlogs
+        ? blogs.find(blog => blog.id === matchBlogs.params.id)
+        : null
 
     return (
         <div>
-            <Notification />
-            {
-                user
-                    ? <><h1>Blogs</h1>
-                        <span>{user.username} logged in </span>
-                        <BlogForm createBlog={handleCreate} />
-                    </>
-                    : <LoginForm createUser={addUser} />
-            }
-            <br />
-            {
-                user
-                    ? blogs.map(blog =>
-                        <Blog key={blog.id} blog={blog} handleLikes={handleLikes} handleDelete={handleDelete} user={user} />
-                    )
-                    : null
-            }
+            <Menu />
+            <Routes>
+                <Route path='/' element={<BlogsList blog={blog} />} />
+                <Route path='/blogs/:id' element={<Blog blog={blog} />} />
+                <Route path='/users' element={<Users />} />
+                <Route path='/users/:id' element={<UserListBlogs blogsUser={blogsUser} />} />
+            </Routes>
         </div>
     )
 }
+
 
 export default App
