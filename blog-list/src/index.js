b7cf9b95d104da08cecd57513b941321f011714e
Micaela Analia Jofre <mkjofre@gmail.com>
2023-02-14 23:02:57 -0300

    Add BrowserRouter- basename

--- blog-list/src/index.js
+++ blog-list/src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
 )
+/* */
